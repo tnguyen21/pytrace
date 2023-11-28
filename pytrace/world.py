0547dacc4cb6a036c5f4e43809287493c539534e
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2023-11-28 11:55:19 -0600

    add default world + test

--- pytrace/world.py
+++ pytrace/world.py
@@ -1,4 +1,20 @@
+from .objects import Sphere
+from .tuple import Color, Point
+from .lighting import PointLight
+from .matrix_transforms import scaling
+
 class World:
     def __init__(self):
         self.objects = []
         self.light_source = None
+    
+    def _init_default_world(self):
+        s1 = Sphere()
+        s1.material.color = Color(0.8, 1.0, 0.6)
+        s1.material.diffuse = 0.7
+        s1.material.specular = 0.2
+        self.objects.append(s1)
+        s2 = Sphere()
+        s2.transform = scaling(0.5, 0.5, 0.5)
+        self.objects.append(s2)
+        self.light_source = PointLight(Point(-10, 10, -10), Color(1, 1, 1))
--- tests/test_world.py
+++ tests/test_world.py
@@ -1,9 +1,22 @@
 import unittest
-import pytrace.tuple
-import pytrace.world
+from pytrace.tuple import Color, Point
+from pytrace.world import World
+from pytrace.matrix_transforms import scaling
+import numpy as np
 
 class WorldTestCase(unittest.TestCase):
     def test_init_world(self):
-        w = pytrace.world.World()
+        w = World()
         self.assertListEqual(w.objects, [])
         self.assertIsNone(w.light_source)
+
+    def test_default_world(self):
+        w = World()
+        w._init_default_world()
+        self.assertEqual(len(w.objects), 2)
+        self.assertEqual(w.objects[0].material.color, Color(0.8, 1.0, 0.6))
+        self.assertEqual(w.objects[0].material.diffuse, 0.7)
+        self.assertEqual(w.objects[0].material.specular, 0.2)
+        self.assertTrue(np.all(w.objects[1].transform == scaling(0.5, 0.5, 0.5)))
+        self.assertEqual(w.light_source.position, Point(-10, 10, -10))
+        self.assertEqual(w.light_source.intensity, Color(1, 1, 1))
